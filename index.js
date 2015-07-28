3fd5612e7fb4ae147de68c1dbbfe57f51eb4b58c
EC2 Default User <ec2-user@ip-172-31-28-147.us-west-2.compute.internal>
2015-07-28 12:39:07 +0000

    first

---
+++ index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+app.use(express.bodyParser());
+app.use(express.static('html')); // serving out static files in directory 'html' 
+
+/* RESTful calls */
+app.get('/test', function(req, res) {
+  res.send("Hello :)");
+});
+
+app.post('/test', function(req, res) {
+  if (!req.body.hasOwnProperty('name')) {
+    res.statusCode = 400;
+    return res.send('Error 400: Post syntax incorrect.');
+  }
+});
+
+/* bind and listen for connections */
+var server = app.listen(3000, function() {
+  console.log('Listening on port %d', server.address().port);
+});
