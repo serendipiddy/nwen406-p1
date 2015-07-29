55ccf95d9f1a6f5742c184b7ff06e3fd5fe1189c
iddy <lemon.and.lime@hotmail.com>
2015-07-30 03:58:24 +1200

    tidied up

--- index.js
+++ index.js
@@ -44,8 +44,8 @@ app.post('/api', function (req, res) {
 
 app.get('/api', function (req, res) {
   return res.send(
-    JSON.stringify(latestData,null,2)
-    +'\n\n/api says: "GOT: <3 <(\'\'<)\n Oh noes! You should be using POST .OTL');
+    '\n\n/api says: "GOT: <3 <(\'\'<)\n Oh noes! You should be using POST .OTL')+
+    JSON.stringify(latestData,null,2);
 });
 
 /* bind and listen for connections */
