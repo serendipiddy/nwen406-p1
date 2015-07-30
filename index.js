7b15e2aa607b7c79ec95a74b13ff34f06fb13246
iddy <lemon.and.lime@hotmail.com>
2015-07-30 14:06:21 +1200

    using Date as UTC

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ app.post('/api', function (req, res) {
     // return res.send('Error 400: POST syntax incorrect.');
   }
   
-  console.log('(new) '+new Date.UTC());
+  console.log('(new) '+new Date.now());
   console.log('(new) JSON Received');
   res.statusCode = 200; // status ok
   res.json({
